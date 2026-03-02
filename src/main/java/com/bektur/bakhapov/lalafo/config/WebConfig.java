eb6900acb4e898c78367c3c5ba6dc00f63a2555b
Bektur Bakhapov <bekturbakhapov@MacBook-Air-Bektur.local>
2026-03-02 18:09:09 +0600

    feature/ added RestTemplate bean with timeout configuration

---
+++ src/main/java/com/bektur/bakhapov/lalafo/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.bektur.bakhapov.lalafo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        SimpleClientHttpRequestFactory simpleClientHttpRequestFactory = new SimpleClientHttpRequestFactory();
+        simpleClientHttpRequestFactory.setConnectTimeout(1000);
+        simpleClientHttpRequestFactory.setReadTimeout(1000);
+        return new RestTemplate(simpleClientHttpRequestFactory);
+    }
+
+}
