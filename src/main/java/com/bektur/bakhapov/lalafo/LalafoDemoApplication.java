9e56c81036e577b0d9ec4d4e41795882867c061a
Bektur Bakhapov <bekturbakhapov@MacBook-Air-Bektur.local>
2026-03-02 16:15:08 +0600

    initial project

---
+++ src/main/java/com/bektur/bakhapov/lalafo/LalafoDemoApplication.java
@@ -0,0 +1,13 @@
+package com.bektur.bakhapov.lalafo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LalafoDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LalafoDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bektur/bakhapov/lalafo/LalafoDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bektur.bakhapov.lalafo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LalafoDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
