066375be2c7c2945cf31f2e91d9fe3b592ddfa8a
Bektur Bakhapov <bekturbakhapov@MacBook-Air-Bektur.local>
2026-03-02 23:05:13 +0600

    feature/ added and realized page ads.html with Thymeleaf

--- src/main/java/com/bektur/bakhapov/lalafo/controller/LalafoController.java
+++ src/main/java/com/bektur/bakhapov/lalafo/controller/LalafoController.java
@@ -2,13 +2,13 @@ package com.bektur.bakhapov.lalafo.controller;
 
 import com.bektur.bakhapov.lalafo.payload.response.ad.AdsPageResponse;
 import com.bektur.bakhapov.lalafo.service.LalafoApiService;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@RestController
+@Controller
 @RequestMapping("/test-result")
 public class LalafoController {
 
@@ -19,12 +19,10 @@ public class LalafoController {
     }
 
     @GetMapping
-    public ResponseEntity<AdsPageResponse> fetchAds() {
-        try {
-            AdsPageResponse response = lalafoApiService.fetchAds();
-            return new ResponseEntity<>(response, HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+    public String fetchAds(@RequestParam(defaultValue = "1") Integer page, Model model) {
+        AdsPageResponse response = lalafoApiService.fetchAds(page);
+        model.addAttribute("ads", response.items());
+        model.addAttribute("currentPage", page);
+        return "ads";
     }
 }
